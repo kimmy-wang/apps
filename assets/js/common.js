7545c99f9b9ae2c623ad3f8f942885a120a63206
upcwangying <upcwangying@126.com>
2020-07-20 18:57:14 +0800

    feat: 持久化

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,16 +1,24 @@
+const WEBSITE_DARK_THEME_KEY = '__wy_apps_color_theme__'
+
 /**
  * 添加暗色主题
  */
 function addDarkTheme() {
     const body = document.getElementById("body");
+    const button = document.getElementById("btn");
     body.classList.add('dark')
+    button.innerText = '白天模式'
+    window.localStorage && localStorage.setItem(WEBSITE_DARK_THEME_KEY, 'dark')
 }
 /**
  * 移除暗色主题
  */
 function removeDarkTheme() {
     const body = document.getElementById("body");
+    const button = document.getElementById("btn");
     body.classList.remove('dark')
+    button.innerText = '黑暗模式'
+    window.localStorage && localStorage.setItem(WEBSITE_DARK_THEME_KEY, 'light')
 }
 /**
  * 切换主题
@@ -26,16 +34,20 @@ const toggleTheme = () => {
 
 function load() {
     let media = window.matchMedia('(prefers-color-scheme: dark)');
+    const darkTheme = (window.localStorage && localStorage.getItem(WEBSITE_DARK_THEME_KEY)) || 'light';
     const body = document.getElementById("body");
     let callback = (e) => {
-        let prefersDarkMode = e.matches;
-        if (prefersDarkMode) {
+        let prefersDarkMode = e && e.matches;
+        if (prefersDarkMode || darkTheme === 'dark') {
             // 搞事情
             addDarkTheme()
         } else {
             removeDarkTheme()
         }
     };
+
+    callback(null)
+
     if (typeof media.addEventListener === 'function') {
         media.addEventListener('change', callback);
     } else if (typeof media.addListener === 'function') {
